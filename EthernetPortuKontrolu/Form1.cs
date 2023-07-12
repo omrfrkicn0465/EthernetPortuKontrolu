8625b42b089df9bf3285be4845d15ae400c2ef88
ömer faruk içen <icenomerfaruk@gmail.com>
2023-07-12 09:57:34 +0300

    Add project files.

---
+++ EthernetPortuKontrolu/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace EthernetPortuKontrolu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckEthernetStatus();
+        }
+        private void CheckEthernetStatus()
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface nic in interfaces)
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet && nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    MessageBox.Show("Ethernet bağlantısı mevcut.");
+                    return;
+                }
+            }
+
+            MessageBox.Show("Ethernet bağlantısı bulunamadı veya bağlantı kapalı.");
+        }
+    }
+}
